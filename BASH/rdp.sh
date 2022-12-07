0a6f2a7b8a78769c8c7964cdfc6aae5b89cd1403
Harvey, Mark <mah0013mobile@gmail.com>
2022-12-07 09:15:27 -0500

    simple script added

---
+++ BASH/rdp.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "[i] Updating and upgrading Kali (this will take a while)"
+apt-get update
+apt-get dist-upgrade -y
+
+echo "[i] Installing Xfce4 & xrdp (this will take a while as well)"
+apt-get install -y kali-desktop-xfce xorg xrdp
+
+echo "[i] Configuring xrdp to listen to port 3390 (but not starting the service)"
+sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
